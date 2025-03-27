e2880c45492d3b396bdccbced95df6f5471c1b28
Elijah Baptiste <elijahbaptiste79@gmail.com>
2025-03-27 02:27:14 -0700

    Created the Empty extension with the logo. Doesn't yet have any password storing capabilities

---
+++ background.js
@@ -0,0 +1,8 @@
+// chrome.action.OnClicked.addListener( tab => {
+//     chrome.scripting.executeScript({
+//         target: {tabId: tab.id },
+//         func: ()  => {
+//             alert('Hello! Right Now, I do not do anything, but soon, I will be storing all of your passwords =)');
+//         }
+//     });
+// });
